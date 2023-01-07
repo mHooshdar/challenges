403c0e8f80d1e2c6db1456a025ccb3a0463a4607
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2023-01-07 09:21:58 +0330

    feat: add solution for two-sum question

---
+++ leetcode/01 - two-sum.js
@@ -0,0 +1,41 @@
+/*
+https://leetcode.com/problems/two-sum/
+Given an array of integers, return the indices of the two numbers that
+added up to a given target
+
+easy
+bruth force - o(n^2) easy
+o(n) easy
+*/
+
+
+// s(1), o(n^2)
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (j === i) continue;
+      if (nums[i] + nums[j] === target) return [i, j]
+    }
+  }
+  return null;
+}
+
+// s(n), o(n)
+function twoSum2(nums, target) {
+  let expected = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (expected[nums[i]] !== undefined) {
+      return [expected[nums[i]], i]
+    }
+    expected[target - nums[i]] = i;
+  }
+  return null;
+}
+
+console.log(twoSum([2,7,11,15], 9))
+console.log(twoSum([3,2,4], 6))
+console.log(twoSum([3,3], 6))
+
+console.log(twoSum2([2,7,11,15], 9))
+console.log(twoSum2([3,2,4], 6))
+console.log(twoSum2([3,3], 6))
