0b0fdefc40d83c24fa62322ce2fd92e779c7f9d2
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2023-01-12 06:40:44 +0330

    feat: add is palindrome question and answer

---
+++ leetcode/05 (e) - is-palindrome.js
@@ -0,0 +1,23 @@
+/*
+Level: easy
+o(n) - easy
+
+https://leetcode.com/problems/valid-palindrome/description/
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string `s`, return `true` if it is a palindrome, or `false` otherwise.
+*/
+
+// s(1), o(n^2)
+/**
+ * @param {number[]} s
+ * @return {boolean}
+ */
+function isPalindrome(s) {
+  const trimedString = s.toLowerCase().replace(/[^a-z0-9]*/gm, '');
+  return trimedString.split('').reverse().join('') === trimedString;
+}
+
+console.log(isPalindrome('A man, a plan, a canal: Panama'));
+console.log(isPalindrome('race a car'));
+console.log(isPalindrome(' '));
