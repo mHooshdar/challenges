0cf2c45aa02a813af94889b390fa4ece9c600bd5
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2023-01-17 08:52:43 +0330

    feat: add minimum remove to make valid p question

---
+++ leetcode/13 (m) - minimum-remove-to-make-valid-parentheses.js
@@ -0,0 +1,26 @@
+/*
+Minimum Remove to Make Valid Parentheses
+Level: easy
+o(n) s(n) - easy
+
+https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+Given a string s of '(' , ')' and lowercase English characters.
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+#medium
+*/
+
+// o(n), s(n)
+/**
+ * @param {string} s
+ * @return {string}
+ */
+function minRemoveToMakeValid(s) {
+  
+}
