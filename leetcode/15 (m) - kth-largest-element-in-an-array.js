bc35ece64eaae64ea69a894d5ab738430bd6f321
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2023-01-19 16:52:48 +0330

    chore: change example

--- leetcode/15 (m) - kth-largest-element-in-an-array.js
+++ leetcode/15 (m) - kth-largest-element-in-an-array.js
@@ -49,5 +49,5 @@ function findKthLargest(nums, k) {
   return sortedArray[sortedArray.length - k];
 }
 
-console.log(quickSortArray([3, 2, 1, 5, 6, 4]), 2);
-console.log(quickSortArray([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
+console.log(findKthLargest([3, 2, 1, 5, 6, 4]), 2);
+console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
