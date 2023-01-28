21254d0a9bcb428a0f9e7c62b646c58424fc5383
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2023-01-28 10:13:22 +0330

    feat: add bfs and dfs 2d array

---
+++ leetcode/23 (m) - numbrer-of-islands.js
@@ -0,0 +1,21 @@
+/*
+Number Of Islands
+Level: medium
+o(m * n) s(m * n) - medium
+
+https://leetcode.com/problems/number-of-islands/
+Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+#medium
+*/
+
+// o(m * n) s(m * n)
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+function numIslands (grid) {
+
+};
