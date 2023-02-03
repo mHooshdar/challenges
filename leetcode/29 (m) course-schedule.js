a7ec5db3812ac7ece66758e6fb7890d653d1b34a
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2023-02-03 09:17:21 +0330

    chore: some typos fixed

--- leetcode/29 (m) course-schedule.js
+++ leetcode/29 (m) course-schedule.js
@@ -1,16 +1,15 @@
 /*
 Course Schedule
 Level: medium
-Answer 1 : Bruth Force - o(p + n^3) s(n^2) - medium
+Answer 1 : Brute Force - o(p + n^3) s(n^2) - medium
 Answer 2 : Topological Sort - o(p + n^2) s(n^2) - medium
-Answer 3 : Topological Sort Without Adj List - o(p + n^2) s(n) - medium
+Answer 3 : Topological Sort Without Adjacent List - o(p + n^2) s(n) - medium
 
 https://leetcode.com/problems/course-schedule/
 There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
 
 For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
 Return true if you can finish all courses. Otherwise, return false.
-
 
 #medium
 */
